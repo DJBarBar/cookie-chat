66a47f388de1d6456b1cd3de9dfb5fb27eb996eb
Cory Barton <coryjbarton@gmail.com>
2017-05-17 16:13:19 -0600

    Added Delete Button

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,31 +1,50 @@
-angular.module('chatroom').controller('mainCtrl', function($scope, messageService) {
+angular.module('chatroom').controller('mainCtrl', function ($scope, messageService) {
+
 
   //The getMessages function will call the getMessages method on the messageService object.
   //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
-
-  $scope.getMessages = function() {
-    messageService.getMessages().then(function(response) {
-      console.log(response);
+  $scope.getMessages = function () {
+    var promise = messageService.getMessages();
+    promise.then(function (response) {
       $scope.messages = response.data;
+
     });
   }
 
 
-  //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
+
+  //The postMessage function will take whatever the user typed in (hint: look at the html and see 
+  //what ng-model correlates to on the input box),
   //pass that text to the postMessage method on the messageService object which will
   //then post it to the backend.
+  $scope.postMessage = function (message) {
 
-  $scope.postMessage = function(message) {
-    messageService.postMessage(message).then(function(response) {
-      console.log(response);
+    messageService.postMessage(message).then(function (reponse) {
+      $scope.messages = reponse.data;
     });
+
+    $scope.message = '';
+
   }
+
 
   //uncomment this code when your getMessages function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-  setInterval(function() {
+  setInterval(function () {
     $scope.getMessages();
 
   }, 1500)
+
+  //Function to display the time properly
+  $scope.time = function (str) {
+    return messageService.time(str);
+  }
+
+  //function to delete all of the messages
+  $scope.deleteMessage = function () {
+    messageService.deleteMessage().then(function (response) {
+      $scope.messages = response.data;
+    })
+  }
 
 })
--- js/messageService.js
+++ js/messageService.js
@@ -1,31 +1,56 @@
-angular.module('chatroom').service('messageService', function($http) {
+angular.module('chatroom').service('messageService', function ($http) {
   //Here you'll need to create two methods. One called postMessage and the other called getMessages.
 
   //On the lines below create a getMessages method. This method will retrieve data from the backend.
   //The url for the get request should be 'https://practiceapi.devmounta.in/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
-
-  this.getMessages = function() {
-    return $http({
-      method: 'GET',
-      url: 'https://practiceapi.devmounta.in/api/chats'
-    });
+  this.getMessages = function () {
+    var promise = $http.get('https://practiceapi.devmountain.com/api/chats');
+    return promise;
   }
 
 
-
-  //On the line below create the postMessage method. This method will add data to the backend server.
-  //The url for the request needs to be 'https://practiceapi.devmounta.in/api/chats'
-  //Because we're making a POST request, need a data object with a message property IE data: {message: "Hello World"}
-  //Also, remember that $http returns a promise. So return the whole $http call (return $http(...)), so you can then use .then in your controller.
-
-  this.postMessage = function(newMessage) {
+  /*
+    //On the line below create the postMessage method. This method will add data to the backend server.
+    //The url for the request needs to be 'https://practiceapi.devmounta.in/api/chats'
+    //Because we're making a POST request, need a data object with a message property 
+    IE data: {message: "Hello World"}
+    //Also, remember that $http returns a promise. So return the whole $http call (return $http(...)), 
+    so you can then use .then in your controller.
+  */
+  this.postMessage = function (message) {
     return $http({
       method: 'POST',
-      url: 'https://practiceapi.devmounta.in/api/chats',
+      url: 'https://practiceapi.devmountain.com/api/chats',
       data: {
-        message: newMessage
+        message: message
       }
     });
   }
+
+
+  this.time = function (str) {
+    var arr = str.split('');
+    var date = [];
+    for (var i = 5; i < 10; i++) {
+      date.push(arr[i]);
+    }
+    date.push('-');
+    for (var i = 0; i < 4; i++) {
+      date.push(arr[i]);
+    }
+    date.push(', ');
+    for (var i = 11; i < 16; i++) {
+      date.push(arr[i]);
+    }
+
+    return date.join('');
+  }
+
+
+  this.deleteMessage = function () {
+    return $http.delete('https://practiceapi.devmountain.com/api/chats');
+  }
+
+
 });
